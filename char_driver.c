9a9305facae6ad8a4458fc7b995b898d9733667c
HameedRahman119 <hameedrahman119@gmail.com>
2026-08-13 09:44:22 +0530

    Harden character driver read/write handling

--- char_driver.c
+++ char_driver.c
@@ -1,89 +1,78 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
 #include <linux/fs.h>
-#include <linux/uaccess.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
 #include <linux/string.h>
+#include <linux/uaccess.h>
 
 #define DEVICE_NAME "mychardev"
 #define BUFFER_SIZE 1024
 
 static int major;
-static char device_buffer[BUFFER_SIZE] =
-"Hello from Kernel Driver\n";
+static char device_buffer[BUFFER_SIZE] = "Hello from Kernel Driver\n";
+static size_t data_size = sizeof("Hello from Kernel Driver\n") - 1;
+static DEFINE_MUTEX(device_lock);
 
-/* Device Open */
-static int dev_open(struct inode *inode,
-                    struct file *file)
+static int dev_open(struct inode *inode, struct file *file)
 {
-    printk(KERN_INFO "Device opened\n");
+    pr_info("%s: device opened\n", DEVICE_NAME);
     return 0;
 }
 
-/* Device Close */
-static int dev_release(struct inode *inode,
-                       struct file *file)
+static int dev_release(struct inode *inode, struct file *file)
 {
-    printk(KERN_INFO "Device closed\n");
+    pr_info("%s: device closed\n", DEVICE_NAME);
     return 0;
 }
 
-/* Read from device */
-static ssize_t dev_read(struct file *file,
-                        char __user *user_buffer,
-                        size_t len,
-                        loff_t *offset)
+static ssize_t dev_read(struct file *file, char __user *user_buffer,
+                        size_t len, loff_t *offset)
 {
-    int bytes_to_read;
+    size_t bytes_to_read;
 
-    if (*offset >= strlen(device_buffer))
+    if (mutex_lock_interruptible(&device_lock))
+        return -ERESTARTSYS;
+
+    if (*offset >= data_size) {
+        mutex_unlock(&device_lock);
         return 0;
+    }
 
-    bytes_to_read = strlen(device_buffer) - *offset;
+    bytes_to_read = min(len, data_size - (size_t)*offset);
 
-    if (len < bytes_to_read)
-        bytes_to_read = len;
-
-    if (copy_to_user(user_buffer,
-                     device_buffer + *offset,
-                     bytes_to_read))
-    {
+    if (copy_to_user(user_buffer, device_buffer + *offset, bytes_to_read)) {
+        mutex_unlock(&device_lock);
         return -EFAULT;
     }
 
     *offset += bytes_to_read;
-
-    printk(KERN_INFO "Data read from device\n");
+    mutex_unlock(&device_lock);
 
     return bytes_to_read;
 }
 
-/* Write to device */
-static ssize_t dev_write(struct file *file,
-                         const char __user *user_buffer,
-                         size_t len,
-                         loff_t *offset)
+static ssize_t dev_write(struct file *file, const char __user *user_buffer,
+                         size_t len, loff_t *offset)
 {
-    if (len > BUFFER_SIZE - 1)
-        len = BUFFER_SIZE - 1;
+    size_t bytes_to_write = min(len, (size_t)(BUFFER_SIZE - 1));
 
-    if (copy_from_user(device_buffer,
-                       user_buffer,
-                       len))
-    {
+    if (mutex_lock_interruptible(&device_lock))
+        return -ERESTARTSYS;
+
+    if (copy_from_user(device_buffer, user_buffer, bytes_to_write)) {
+        mutex_unlock(&device_lock);
         return -EFAULT;
     }
 
-    device_buffer[len] = '\0';
+    device_buffer[bytes_to_write] = '\0';
+    data_size = bytes_to_write;
+    mutex_unlock(&device_lock);
 
-    printk(KERN_INFO "Data written to device: %s\n",
-           device_buffer);
-
-    return len;
+    pr_info("%s: %zu bytes written\n", DEVICE_NAME, bytes_to_write);
+    return bytes_to_write;
 }
 
-/* File operations */
-static struct file_operations fops =
-{
+static const struct file_operations fops = {
     .owner = THIS_MODULE,
     .open = dev_open,
     .read = dev_read,
@@ -91,44 +80,28 @@ static struct file_operations fops =
     .release = dev_release,
 };
 
-/* Module Init */
 static int __init char_driver_init(void)
 {
-    major = register_chrdev(0,
-                            DEVICE_NAME,
-                            &fops);
-
-    if (major < 0)
-    {
-        printk(KERN_ALERT
-               "Driver registration failed\n");
-
+    major = register_chrdev(0, DEVICE_NAME, &fops);
+    if (major < 0) {
+        pr_err("%s: registration failed: %d\n", DEVICE_NAME, major);
         return major;
     }
 
-    printk(KERN_INFO
-           "Character driver registered\n");
-
-    printk(KERN_INFO
-           "Major number = %d\n",
-           major);
-
+    pr_info("%s: registered with major number %d\n", DEVICE_NAME, major);
     return 0;
 }
 
-/* Module Exit */
 static void __exit char_driver_exit(void)
 {
-    unregister_chrdev(major,
-                      DEVICE_NAME);
-
-    printk(KERN_INFO
-           "Character driver removed\n");
+    unregister_chrdev(major, DEVICE_NAME);
+    pr_info("%s: unregistered\n", DEVICE_NAME);
 }
 
 module_init(char_driver_init);
 module_exit(char_driver_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("OpenAI");
-MODULE_DESCRIPTION("Simple Character Driver");
+MODULE_AUTHOR("HameedRahman119");
+MODULE_DESCRIPTION("Simple Linux character driver with read/write support");
+MODULE_VERSION("1.1");
